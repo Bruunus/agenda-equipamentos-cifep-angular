f57694994d52717580d867109e886b214bdd11d9
Bruno Fernandes <brunus.devbackfront@gmail.com>
2024-03-24 19:20:44 -0300

    alterado suporte de folha de estido para scss e feito teste e instalado bootstrap

--- src/app/reservas/reservas.component.ts
+++ src/app/reservas/reservas.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-reservas',
   templateUrl: './reservas.component.html',
-  styleUrls: ['./reservas.component.sass']
+  styleUrls: ['./reservas.component.scss']
 })
 export class ReservasComponent implements OnInit {
 
