61a1859b824d40a509e88aef64d053051eb5795b
Bruno <brunus.desenvolvimentobackfront@gmail.com>
2024-04-03 17:22:51 -0300

    criado definição superficial da tela de carregamento de reservas

--- src/app/reservas/reservas.component.ts
+++ src/app/reservas/reservas.component.ts
@@ -8,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReservasComponent implements OnInit {
 
+  horaRetiradaFormatter: any;
+  horaDevolucaoFormatter: any;
   reservas: any[] = [];
+
+ 
 
   constructor(private http: HttpClient) {}
 
@@ -17,7 +21,7 @@ export class ReservasComponent implements OnInit {
     this.loadDataOfDay();
   }
 
-
+  data: any;
 
   loadDataOfDay() {
 
@@ -26,6 +30,27 @@ export class ReservasComponent implements OnInit {
         (reservas: any[]) => {
           this.reservas = reservas;
           console.log('Reservas do dia:', this.reservas);
+          
+           this.reservas.forEach(timerFormatter => {
+            
+             
+            //const horaRetirada = timerFormatter.agenda[0].dataDevolucao;
+
+            timerFormatter.agenda.forEach((timer:any) => {
+              this.horaRetiradaFormatter = timer.horaRetirada.split(':').slice(0,2).join(':');
+              this.horaDevolucaoFormatter = timer.horaDevolucao.split(':').slice(0, 2).join(':');
+              
+
+
+              console.log(this.horaDevolucaoFormatter)
+            })
+             
+
+            //console.log(horaRetirada)
+
+            
+           })
+
         },
         (error) => {
           console.error('Erro ao carregar as reservas do dia:', error);
